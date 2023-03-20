e3783c53a5d3bc4c0bd13781d067ffd41d3c25a2
pmhxhsj <minho7952@naver.com>
2023-03-20 20:36:09 +0900

    feat: Footer 정의

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { css } from "@emotion/react";
+import { flexGrid } from "@/emotion/grid";
+import { Typography } from "nimble-nds";
+
+const footerStyle = css`
+  background-color: black;
+  height: 10rem;
+  width: 100%;
+`;
+
+const Footer = () => (
+  <footer
+    css={css(
+      flexGrid({
+        direction: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }),
+      footerStyle
+    )}
+  >
+    <Typography color="gray100" weight="lg" size="sm">
+      footer
+    </Typography>
+  </footer>
+);
+
+export default Footer;
