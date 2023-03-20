7b4badf5df7aa6d844c70b8cae3fbbbe0b3f328c
pmhxhsj <minho7952@naver.com>
2023-03-20 20:36:40 +0900

    feat: FlexGrid 수정 - gap 인자 받도록 수정

--- src/emotion/grid.ts
+++ src/emotion/grid.ts
@@ -5,13 +5,16 @@ export const flexGrid = ({
   direction,
   justifyContent,
   alignItems,
+  gap,
 }: {
   direction: string;
   justifyContent: string;
   alignItems: string;
+  gap?: string;
 }) => css`
   display: flex;
   flex-direction: ${direction};
   justify-content: ${justifyContent};
   align-items: ${alignItems};
+  gap: ${gap};
 `;
