68b8830bf1fe1845ce8de6439be792ef6cd6af89
pmhxhsj <minho7952@naver.com>
2023-03-20 20:37:15 +0900

    feat: 상수 정의

---
+++ src/components/Landing/constants/index.ts
@@ -0,0 +1,34 @@
+const EXPLANE = [
+  {
+    title: "첫 번째 설명",
+    description: "dasdasdasdasdasdasdas",
+  },
+  {
+    title: "두 번째 설명",
+    description: "dasdasdasdasdasdasdas",
+  },
+  {
+    title: "세 번째 설명",
+    description: "dasdasdasdasdasdasdas",
+  },
+];
+
+const PRICING = [
+  {
+    type: "Free",
+    description: "For a individual devs",
+    benefits: ["5 GB storage", "10 GB transfer", "Maximum 5 users"],
+  },
+  {
+    type: "Premium",
+    description: "For a small team",
+    benefits: ["5 GB storage", "10 GB transfer", "Maximum 5 users"],
+  },
+  {
+    type: "Ultimate",
+    description: "For a large componies",
+    benefits: ["5 GB storage", "10 GB transfer", "Maximum 5 users"],
+  },
+];
+
+export default { EXPLANE, PRICING };
