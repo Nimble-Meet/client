235124cf0c8bcbc255ce8ab986638cc554afbac3
pmhxhsj <minho7952@naver.com>
2023-03-22 23:23:32 +0900

    feat: ExplaneCard => 정의한 emotion 사용

--- src/components/Landing/subcomponents/ExplaneCard/index.tsx
+++ src/components/Landing/subcomponents/ExplaneCard/index.tsx
@@ -5,39 +5,14 @@ import { css } from '@emotion/react';
 import { explaneCardStyle, introIconStyle } from './explaneCard.styles';
 import { Typography } from 'nimble-nds';
 
-const ExplaneCardStyle = css`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    height: 300px;
-
-    padding: 2rem;
-
-    position: relative;
-
-    border-radius: 2rem;
-    background-color: black;
-    border: 2px solid #393939;
-`;
-
-const Image = css`
-    height: 100px;
-    width: 100px;
-
-    border-radius: 1rem;
-    background-color: #393939;
-    border: none;
-`;
-
 type Props = {
     title: string;
     description: string;
 };
 
 const ExplaneCard = ({ title, description }: Props) => (
-    <div css={ExplaneCardStyle}>
-        <div css={Image} />
+    <div css={explaneCardStyle}>
+        <div css={introIconStyle} />
         <div>
             <Typography color="gray50" size="24px" weight="lg" value={title} />
         </div>
