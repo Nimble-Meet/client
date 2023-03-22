91aeb6e525b7e31c781da979c75b71f4c72ff814
pmhxhsj <minho7952@naver.com>
2023-03-22 23:27:44 +0900

    feat: flex-grid type 정의

--- src/styles/grid.ts
+++ src/styles/grid.ts
@@ -1,17 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
+type FlexGridProps = {
+    direction: 'row' | 'column';
+    justifyContent: 'center' | 'flex-start' | 'flex-end' | 'space-between';
+    alignItems: 'center' | 'flex-start' | 'flex-end' | 'space-between';
+    gap?: string;
+};
+
 export const flexGrid = ({
     direction,
     justifyContent,
     alignItems,
     gap
-}: {
-    direction: string;
-    justifyContent: string;
-    alignItems: string;
-    gap?: string;
-}) => css`
+}: FlexGridProps) => css`
     display: flex;
     flex-direction: ${direction};
     justify-content: ${justifyContent};
