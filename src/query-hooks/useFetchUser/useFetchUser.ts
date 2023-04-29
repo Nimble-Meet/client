d1e8f26c92c4e09556e639ffb37bdbc6c292d7ae
pmhxhsj <minho7952@naver.com>
2023-04-29 17:49:54 +0900

    feat: 임시 react-query 적용

--- src/query-hooks/useFetchUser/useFetchUser.ts
+++ src/query-hooks/useFetchUser/useFetchUser.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from 'react-query';
 
-import { createUser } from './apis';
+import { createUser, checkLogin } from './apis';
 
 import USER_KEY from './keys';
 
@@ -9,9 +9,23 @@ import { IUser } from '@/types/user';
 export const useCreateUser = () => {
     const queryClient = useQueryClient();
 
-    return useMutation(({ nickname, email, password}: IUser) => createUser({ nickname, email, password}), {
-        onSuccess: (data) => {
-            console.log(data);
+    return useMutation(
+        ({ nickname, email, password }: IUser) =>
+            createUser({ nickname, email, password }),
+        {
+            onSuccess: (data) => {
+                console.log(data);
+            }
         }
-    })
-}
+    );
+};
+
+export const useCheckLogin = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation(() => checkLogin(), {
+        onSuccess: (data) => {
+            queryClient.setQueryData(USER_KEY.all, data);
+        }
+    });
+};
