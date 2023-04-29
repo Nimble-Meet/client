e695bfd6b6cfca1b79f97189351e2171c128bdf0
pmhxhsj <minho7952@naver.com>
2023-04-29 17:49:16 +0900

    refactor: Layouts => layouts 으로 폴더 명 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Landing from '@/Layouts/Landing';
+import Landing from '@/layouts/Landing';
 
 export default function Home() {
     return (
