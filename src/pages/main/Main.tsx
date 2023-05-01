af65455ab2bd45d1da2f1ea1ec309aceee1d0adc
pmhxhsj <minho7952@naver.com>
2023-05-01 15:07:49 +0900

    feat: 메인 페이지 정의

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,31 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import { css } from '@emotion/react';
 
-const Main = () => <main>hihi</main>;
+import { FlexContainer } from 'nimble-ds';
+
+import Navigation from './components/Navigation';
+import MainContainer from './components/MainContainer';
+import CurrentTimeContainer from './components/CurrentTimeContainer';
+
+const Main = () => {
+    return (
+        <main>
+            <Navigation />
+            <FlexContainer
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                gap="3rem"
+                customCss={css`
+                    height: calc(100vh - 8rem);
+                `}
+            >
+                <MainContainer />
+                <CurrentTimeContainer />
+            </FlexContainer>
+        </main>
+    );
+};
 
 export default Main;
