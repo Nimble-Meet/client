7baff15d846f16db4ff54b6cc4cd972bd1f6f26b
pmhxhsj <minho7952@naver.com>
2023-05-01 15:07:15 +0900

    feat: Main 기능 정의
    - 새로운 회의, 참가 등

---
+++ src/pages/main/components/MainContainer/subcomponents/ActionButton/ActionButton.style.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export const actionButtonStyle = css`
+    background-color: #ededed;
+    width: 10rem;
+    height: 10rem;
+
+    padding: 1rem;
+
+    border-radius: 2rem;
+`;
