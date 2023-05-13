32f351027ca5ee81b2ccc5e243b40a62a7f773cc
pmhxhsj <minho7952@naver.com>
2023-05-14 02:01:26 +0900

    feat: accessToken, component 단 requireAuth에 따라 로그인 페이지로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,38 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
-import "../assets/font/font.css";
+import '../assets/font/font.css';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 0,
-    },
-  },
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false
+        }
+    }
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </RecoilRoot>
-  );
+    const router = useRouter();
+    const token = Cookies.get('accessToken');
+
+    React.useEffect(() => {
+        if (!token && (Component as any).requireAuth) {
+            router.push('/auth/signIn');
+        }
+    }, [router.pathname, token]);
+
+    return (
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </RecoilRoot>
+    );
 }
