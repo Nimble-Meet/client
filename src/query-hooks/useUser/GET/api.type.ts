16d2fa20e44492899831253b68e18f25ea763f4d
pmhxhsj <minho7952@naver.com>
2023-05-14 02:00:02 +0900

    feat: useUser GET 타입 재정의

--- src/query-hooks/useUser/GET/api.type.ts
+++ src/query-hooks/useUser/GET/api.type.ts
@@ -1,19 +1,15 @@
 // checkUserLoginStatus function Type
 export namespace CheckUserLoginStatus {
     export interface Response {
-        data: {
-            email: string;
-            nickname: string;
-            providerType: 'LOCAL' | 'GOOGLE' | 'KAKAO' | 'NAVER';
-        };
+        email: string;
+        nickname: string;
+        providerType: 'LOCAL' | 'GOOGLE' | 'KAKAO' | 'NAVER';
     }
 
     export interface Return {
-        data: {
-            email: string;
-            nickname: string;
-            providerType: 'LOCAL' | 'GOOGLE' | 'KAKAO' | 'NAVER';
-        };
+        email: string;
+        nickname: string;
+        providerType: 'LOCAL' | 'GOOGLE' | 'KAKAO' | 'NAVER';
     }
 
     export interface GetFunc {
