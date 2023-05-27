a79c4852e181fc63badbeac07523e5e87b965946
pmhxhsj <minho7952@naver.com>
2023-05-27 17:56:43 +0900

    feat(style): explaneCard 스타일 수정

--- src/Layouts/Landing/subcomponents/ExplaneCard/ExplaneCard.style.tsx
+++ src/Layouts/Landing/subcomponents/ExplaneCard/ExplaneCard.style.tsx
@@ -6,16 +6,24 @@ export const explaneCardStyle = css`
     position: relative;
     height: 300px;
 
+    flex: 1;
+
     padding: 2rem;
 
     border-radius: 2rem;
-    background-color: black;
+    background-color: ${COLOR.softBlack};
     border: 2px solid ${COLOR.gray900};
+
+    box-shadow: 0px 0px 8px ${COLOR.gray900};
 `;
 
 export const introIconStyle = css`
     height: 100px;
     width: 100px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     border-radius: 1rem;
     background-color: ${COLOR.gray900};
