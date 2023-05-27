edb1ec89fb579d34aa54cf7aca375360de3643cd
pmhxhsj <minho7952@naver.com>
2023-05-28 02:30:34 +0900

    feat: OauthButton 컴포넌트 정의

---
+++ src/pages/auth/signIn/subcomponents/OAuthButton/OAuthButton.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+import { FlexContainer } from 'nimble-ds';
+
+import { oauthButtonStyle } from './OAuthButton.style';
+
+interface Props {
+    src: string;
+    bgColor: string;
+}
+
+const OAuthButton = ({ src, bgColor }: Props) => {
+    return (
+        <FlexContainer
+            justifyContent="center"
+            alignItems="center"
+            customCss={oauthButtonStyle(bgColor)}
+        >
+            <Image src={src} alt="카카오 로그인 버튼" width={24} height={24} />
+        </FlexContainer>
+    );
+};
+
+export default OAuthButton;
