509d33198954c639319c664d28261ebdc785a7c7
pmhxhsj <minho7952@naver.com>
2023-05-31 23:24:15 +0900

    feat: AuthContainer 정의
    - Auth관련 페이지(signIn, signUp)에서 왼쪽 Auth관련 container 역할

---
+++ src/pages/auth/subcomponents/AuthContainer/AuthContainer.tsx
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+import AuthHeader from './subcomponents/AuthHeader';
+
+import { FlexContainer } from 'nimble-ds';
+
+import { authContainerStyle } from './AuthContainer.style';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const AuthContainer = ({ children }: Props) => {
+    return (
+        <section css={authContainerStyle}>
+            <AuthHeader />
+            <FlexContainer
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                basis="100vh"
+                gap="1.5rem"
+            >
+                {children}
+            </FlexContainer>
+        </section>
+    );
+};
+
+export default AuthContainer;
