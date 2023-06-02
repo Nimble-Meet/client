af9ce90f0da24f31bb23d54070a66e575d02f8da
pmhxhsj <minho7952@naver.com>
2023-06-03 00:57:00 +0900

    feat: Landing관련 컴포넌트 components/Landing에 정의
    - @subComponents로 depth 구분

---
+++ src/components/Landing/@subComponents/ExplaneCard/ExplaneCard.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import COLOR from '@/common/color';
+
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+
+import { Typography, FlexContainer } from 'nimble-ds';
+
+// emotion styles
+import { explaneCardStyle, introIconStyle } from './ExplaneCard.style';
+
+// animation styles
+import {
+    opacityToHundred,
+    opacityZeroToHundred
+} from '@/common/animation/opacity';
+
+import { imageSizeUp } from '@/common/animation/size';
+
+type Props = {
+    title: string;
+    description: string;
+    trasitionDelay: number;
+    icon: React.ComponentType<any>;
+};
+
+const ExplaneCard = ({ title, description, icon, trasitionDelay }: Props) => {
+    const Icon = icon;
+
+    const { targetRef: imageRef, isIntersecting: imageInterscting } =
+        useIntersectionObserver(0);
+
+    const { targetRef: titleRef, isIntersecting: titleInterscting } =
+        useIntersectionObserver(0);
+
+    React.useEffect(() => {
+        if (imageInterscting && imageRef.current) {
+            imageRef.current.style.transform = 'scale(1)';
+        } else if (imageRef.current) {
+            imageRef.current.style.transform = 'scale(0.8)';
+        }
+    }, [imageInterscting]);
+
+    const {
+        targetRef: descriptionRef,
+        isIntersecting: descriptionInterscting
+    } = useIntersectionObserver(0);
+
+    return (
+        <FlexContainer
+            direction="column"
+            gap="1rem"
+            grow={1}
+            customCss={[explaneCardStyle]}
+        >
+            <div
+                ref={imageRef}
+                css={[
+                    introIconStyle,
+                    imageInterscting && imageSizeUp,
+                    css`
+                        transition-delay: ${trasitionDelay}s !important;
+                    `
+                ]}
+            >
+                <Icon color={COLOR.gray50} size="48px" />
+            </div>
+            <div ref={titleRef} css={titleInterscting && opacityToHundred}>
+                <Typography
+                    color="gray50"
+                    size="24px"
+                    weight="lg"
+                    value={title}
+                />
+            </div>
+            <div
+                ref={descriptionRef}
+                css={descriptionInterscting && opacityZeroToHundred}
+            >
+                <Typography
+                    color="gray500"
+                    size="16px"
+                    weight="lg"
+                    value={description}
+                />
+            </div>
+        </FlexContainer>
+    );
+};
+
+export default ExplaneCard;
---
+++ src/components/Landing/@subComponents/PricingCard/PricingCard.tsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import COLOR from '@/common/color';
+
+import { BsCheck } from 'react-icons/bs';
+import { TbCurrencyDollar } from 'react-icons/tb';
+
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+
+import { Button, Typography, FlexContainer } from 'nimble-ds';
+
+// emotion styles
+import {
+    pricingCardContainerStyle,
+    pricingPlanStyle
+} from './PricingCard.style';
+
+// animation styles
+import { smoothVertical, smoothHorizontal } from '@/common/animation/move';
+import { opacityZeroToHundred } from '@/common/animation/opacity';
+
+type Props = {
+    type: string;
+    description: string;
+    benefits: string[];
+    price: string;
+    disabled: boolean;
+    animationDelay: number;
+    moveMainPage: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
+
+const PricingCard = ({
+    type,
+    description,
+    benefits,
+    price,
+    disabled,
+    animationDelay,
+    moveMainPage
+}: Props) => {
+    const { targetRef: titleRef, isIntersecting: titleInterscting } =
+        useIntersectionObserver(0);
+
+    const {
+        targetRef: descriptionRef,
+        isIntersecting: descriptionInterscting
+    } = useIntersectionObserver(0);
+
+    const { targetRef: benefitsRef, isIntersecting: benefitsInterscting } =
+        useIntersectionObserver(0);
+
+    return (
+        <FlexContainer
+            direction="column"
+            gap="1rem"
+            grow={1}
+            customCss={pricingCardContainerStyle}
+        >
+            <div
+                css={[
+                    pricingPlanStyle,
+                    css`
+                        display: flex;
+                        flex-direction: column;
+                        gap: 0.25rem;
+                        animation-delay: ${animationDelay}s !important;
+                    `
+                ]}
+            >
+                <div
+                    ref={titleRef}
+                    css={titleInterscting && smoothVertical('-1rem')}
+                >
+                    <Typography
+                        color="gray50"
+                        size="40px"
+                        weight="lg"
+                        value={type}
+                    />
+                </div>
+                <div
+                    ref={descriptionRef}
+                    css={descriptionInterscting && opacityZeroToHundred}
+                >
+                    <Typography
+                        color="gray500"
+                        size="16px"
+                        weight="lg"
+                        value={description}
+                    />
+                </div>
+            </div>
+            <FlexContainer
+                direction="column"
+                gap="1rem"
+                ref={benefitsRef}
+                customCss={[
+                    css`
+                        margin-bottom: 2rem;
+                    `,
+                    benefitsInterscting ? smoothHorizontal('-1rem') : css``
+                ]}
+            >
+                {benefits.map((benefit, i) => (
+                    <div
+                        key={i}
+                        css={css`
+                            display: flex;
+                            gap: 0.25rem;
+                        `}
+                    >
+                        <BsCheck color={COLOR.gray600} size="1.5rem" />
+                        <Typography
+                            color="gray300"
+                            size="18px"
+                            weight="md"
+                            value={`${benefit}`}
+                        />
+                    </div>
+                ))}
+            </FlexContainer>
+            <div
+                css={css`
+                    display: flex;
+                    align-items: center;
+                    gap: 0.5rem;
+                `}
+            >
+                <TbCurrencyDollar color={COLOR.gray600} size="2rem" />
+                <Typography
+                    color="gray50"
+                    size="32px"
+                    weight="lg"
+                    value={price}
+                />
+                <Typography
+                    color="gray500"
+                    size="24px"
+                    weight="lg"
+                    value="/ month"
+                />
+            </div>
+            <Button
+                size="lg"
+                theme="link"
+                disabled={disabled}
+                fontSize="1rem"
+                width="100%"
+                onClick={moveMainPage}
+                round
+            >
+                {disabled ? 'Coming soon' : '시작하기'}
+            </Button>
+        </FlexContainer>
+    );
+};
+
+export default PricingCard;
