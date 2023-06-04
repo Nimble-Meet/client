54341b98940b7f30dcad65e9a3295bcd3571ba7c
pmhxhsj <minho7952@naver.com>
2023-06-04 15:57:33 +0900

    feat: app.tsx 수정
    - TODO: 추후 로그인이 필요한 페이지 미들웨어 개발로 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
@@ -19,15 +17,6 @@ const queryClient = new QueryClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-    const router = useRouter();
-    const token = Cookies.get('accessToken');
-
-    React.useEffect(() => {
-        if (!token && (Component as any).isRequireAuthPage) {
-            router.push('/auth/signIn');
-        }
-    }, [router.pathname, token]);
-
     return (
         <RecoilRoot>
             <QueryClientProvider client={queryClient}>
