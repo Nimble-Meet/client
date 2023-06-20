5f826f9ebcdb70e32c907ccaf5b0b8bb83dc5c40
pmhxhsj <minho7952@naver.com>
2023-06-20 23:27:17 +0900

    chore: next.config, tsconfig

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `http://${process.env.REACT_APP_SERVER_DOMAIN_URL}/api/:path*`,
-      },
-    ];
-  },
+    reactStrictMode: true,
+    experimental: { appDir: true },
+    swcMinify: true,
+    output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `http://${process.env.REACT_APP_SERVER_DOMAIN_URL}/api/:path*`
+            }
+        ];
+    }
 };
 
 module.exports = nextConfig;
