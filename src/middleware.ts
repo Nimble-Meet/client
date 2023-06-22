dec40eef33e39c7031d9a53fa2b59f65e7cd9d7c
pmhxhsj <minho7952@naver.com>
2023-06-22 22:58:44 +0900

    feat: 로그인 검증 미들웨어 구현
    - 로그인일 때 로그인 페이지 접근 시 메인페이지로
    - 로그인이 아닐 때 로그인이 필요한 페이지 접근 시 로그인 페이지로

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    const cookie = request.cookies.get('accessToken')?.value;
+
+    if (request.nextUrl.pathname.startsWith('/main') && !cookie) {
+        return NextResponse.redirect(new URL('/auth/signIn', request.url));
+    }
+
+    if (request.nextUrl.pathname.startsWith('/auth/signIn') && cookie) {
+        return NextResponse.redirect(new URL('/main', request.url));
+    }
+}
+
+export const config = {
+    matcher: ['/main', '/auth/signIn']
+};
