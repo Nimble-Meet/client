35517517a37063ff71ed844e3d51d60d45cd3c91
pmhxxhsj <minho7952@naver.com>
2023-07-19 22:54:26 +0900

    feat: Next middleware 메인 페이지 로그인 검증 부분 삭제
    - 메인 페이지는 누구나 접근 가능 / 로그인 해야 기능 이용 가능
    - 로그인 검증 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,11 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
     const cookie = request.cookies.get('access_token')?.value;
 
-    if (request.nextUrl.pathname.startsWith('/main') && !cookie) {
-        return NextResponse.redirect(new URL('/auth/signIn', request.url));
-    }
-
     if (request.nextUrl.pathname.startsWith('/auth/signIn') && cookie) {
-        return NextResponse.redirect(new URL('/main', request.url));
+        return NextResponse.redirect(new URL('/', request.url));
     }
 }
 
 export const config = {
-    matcher: ['/main', '/auth/signIn']
+    matcher: ['/', '/auth/signIn']
 };
