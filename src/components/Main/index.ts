86a9868e9464d5dd49de0427f05b4dd2bc0830a5
pmhxxhsj <minho7952@naver.com>
2023-07-26 00:27:37 +0900

    feat: 생성한 Main 관련 컴포넌트 export

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -1,5 +1,7 @@
 export { default as ActionButton } from './ActionButton/ActionButton';
 export { default as CurrentTimeContainer } from './CurrentTimeContainer/CurrentTimeContainer';
-export { default as MainContainer } from './MainContainer/MainContainer';
+export { default as CreateMeetingContainer } from './CreateMeetingContainer/CreateMeetingContainer';
 export { default as Navigation } from './Navigation/Navigation';
 export { default as NavigationButton } from './NavigationButton/NavigationButton';
+export { default as CreateMeetModal } from './CreateMeetModal/CreateMeetModal';
+export { default as LocalNavigation } from './LocalNavigation/LocalNavigation';
