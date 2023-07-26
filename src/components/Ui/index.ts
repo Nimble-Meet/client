1a1605e38a9a6c04af08f34aacd397e4e53bb553
pmhxxhsj <minho7952@naver.com>
2023-07-26 22:04:49 +0900

    feat: Ui index.ts에 export

--- src/components/Ui/index.ts
+++ src/components/Ui/index.ts
@@ -2,3 +2,5 @@
 export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
 export { default as Divider } from './Divider/Divider';
+export { default as Loader } from './Loader/Loader';
+export { default as InputContainer } from './InputContainer/InputContainer';
