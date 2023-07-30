3743f6542e53979dd53e3007a866a98ce351069b
pmhxxhsj <minho7952@naver.com>
2023-07-30 16:21:04 +0900

    feat: MeetingDashboard 정의

---
+++ src/components/Main/MeetingDashboard/dummy.ts
@@ -0,0 +1,188 @@
+import imgSrc from './parkminho.png';
+
+export const DUMMY = [
+    {
+        id: '1',
+        title: '0730 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 코드 컨벤션 및 디자인 시스템에 대한 기획입니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '2',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '3',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '4',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '5',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '6',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '7',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    },
+    {
+        id: '8',
+        title: '0729 Nimble 회의록',
+        description:
+            '금일 회의 주제는 Nimble의 일정 관리입니다. 프로젝트 기간과 관련한 변경사항들을 기록하며 회의를 진행합니다.',
+        peoples: [
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            },
+            {
+                email: 'hihi',
+                nickname: 'hihi',
+                imgSrc
+            }
+        ]
+    }
+];
