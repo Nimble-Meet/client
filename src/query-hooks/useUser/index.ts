7803c0b444a563665d7814331eedefe685ae2f60
pmhxxhsj <minho7952@naver.com>
2023-08-01 20:43:11 +0900

    feat: useUser 관련 react-query 재정의

--- src/query-hooks/useUser/index.ts
+++ src/query-hooks/useUser/index.ts
@@ -1,7 +1,37 @@
-import GET from './GET';
-import POST from './POST';
+import { AxiosError } from 'axios';
+import { useQuery, useMutation } from 'react-query';
 
-export default {
-    GET,
-    POST
+// apis
+import { getUserInfo, checkLoginStatus, createNewUser } from './api';
+
+// query key
+import { userKey } from './key';
+
+import type { Get, Post } from './api.type';
+
+// [GET] 특정 사용자의 정보를 가져옴
+const useGetUserInfo = (email: string) =>
+    useQuery<Get.UserInfo.Return>(userKey.info(email), () =>
+        getUserInfo({ email })
+    );
+
+// [GET] 로그인 상태를 확인함
+const useFetchLoginStatus = () =>
+    useQuery<Get.CheckUser.Return>(userKey.all(), checkLoginStatus, {
+        suspense: true
+    });
+
+// [POST] 새로운 사용자를 만듦
+const useCreateNewUser = () => {
+    return useMutation<Post.NewUser.Return, AxiosError, Post.NewUser.ReqParams>(
+        ({ nickname, email, password }) =>
+            createNewUser({ nickname, email, password }),
+        {
+            onSuccess: (data: Post.NewUser.Return) => {
+                return Promise.resolve(data);
+            }
+        }
+    );
 };
+
+export { useGetUserInfo, useFetchLoginStatus, useCreateNewUser };
---
+++ src/query-hooks/useUser/key.ts
@@ -0,0 +1,7 @@
+const userKey = {
+    all: () => ['user'],
+    // 유저 정보 관련 key
+    info: (email: string) => [...userKey.all(), email]
+};
+
+export { userKey };
