d28da03a74350b75c762b8469db5a78c5b59acd8
pmhxxhsj <minho7952@naver.com>
2023-09-04 23:37:34 +0900

    feat: 버젼업 react-query Provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import '@/styles/globals.css';
 
 import { Toaster } from 'react-hot-toast';
 import { ErrorBoundary } from '@/components/Error';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 import '../assets/font/font.css';
@@ -30,15 +30,15 @@ export default function RootLayout({
         <html>
             <head />
             <body>
-                <RecoilRoot>
-                    <QueryClientProvider client={queryClient}>
+                <QueryClientProvider client={queryClient}>
+                    <RecoilRoot>
                         <ErrorBoundary>
                             {children}
                             <Toaster />
                         </ErrorBoundary>
                         <ReactQueryDevtools initialIsOpen={false} />
-                    </QueryClientProvider>
-                </RecoilRoot>
+                    </RecoilRoot>
+                </QueryClientProvider>
             </body>
         </html>
     );
