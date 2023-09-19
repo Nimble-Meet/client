3497341cd1a5e3a2c11769aed8cbd31ce30c268c
박민호 <goorm@kennyPark.local>
2023-09-19 21:04:16 +0900

    feat: 미팅 입장하면 서버 사이드로 미팅 데이터 가져오도록 로직 구현

---
+++ src/app/meeting/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { fetcher } from "@/app/api/fetcher";
+
+import MeetingPage from "@/view/meeting";
+
+const fetchMeeting = async (meetingId: string) => {
+    const response = await fetcher('GET', `/api/meet/${meetingId}`);
+
+    return response;
+}
+
+const Meeting = async (response: any) => {
+    const { id: meetingId } = response.params;
+
+    const meetingData = await fetchMeeting(meetingId);
+
+    return <MeetingPage meetingData={meetingData} />
+}
+
+export default Meeting;
---
+++ src/view/meeting/index.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Meeting = ({ meetingData }: { meetingData : any}) => {
+    console.log(meetingData);
+    
+    return <div>21312412412</div>
+}
+
+export default Meeting;
