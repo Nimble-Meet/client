cb21aded1aa09fac6e747baa9b193fe391bdb78b
박민호 <goorm@kennyPark.local>
2023-09-25 19:04:48 +0900

    feat: .env 초기화

--- src/app/api/fetcher.ts
+++ src/app/api/fetcher.ts
@@ -11,7 +11,7 @@ export const fetcher = async (url: string, options = {}) => {
         }
     };
 
-    const response = await fetch(`http://localhost:3000${url}`, {
+    const response = await fetch(`${process.env.CLIENT_BASE_URL}${url}`, {
         ...defaultOptions,
         ...options
     });
